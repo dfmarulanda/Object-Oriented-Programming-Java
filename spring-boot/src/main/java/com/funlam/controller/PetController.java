f202f9053572a573eb0b9974b5ef8930de02173a
Daniel Marulanda <d@dondo.com>
2024-09-30 11:28:53 -0500

    se arregla el petservice

--- spring-boot/src/main/java/com/funlam/controller/PetController.java
+++ spring-boot/src/main/java/com/funlam/controller/PetController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.funlam.poo.service.VirtualPetGameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.funlam.poo.model.Pet;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -16,12 +15,11 @@ public class PetController {
     @Autowired
     private VirtualPetGameService virtualPetGameService;
 
-    @GetMapping("/{id}")
+    @GetMapping
     public Pet getPet(){
         return virtualPetGameService.getPet();
     }
-	@GetMapping("/greeting")
-    @PostMapping
+    @PostMapping("/interact")
     public void interactWithPet(){
         virtualPetGameService.specialAction();
     }
